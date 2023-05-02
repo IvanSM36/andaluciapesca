98738d1252c71445054a2050da51c6894ed441c8
IvanSM36 <ivan_sm@hotmail.es>
2023-05-03 00:25:07 +0200

    Se corrige la estructura de diseño

--- lib/src/cuenta/perfil_usuario.dart
+++ lib/src/cuenta/perfil_usuario.dart
@@ -19,6 +19,7 @@ class _PerfilUsuarioState extends State<PerfilUsuario> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        ///// APPBAR /////
         appBar: AppBar(
           leading: IconButton(
             icon: const Icon(Icons.arrow_back_ios),
@@ -62,133 +63,182 @@ class _PerfilUsuarioState extends State<PerfilUsuario> {
             ),
           ],
         ),
+
+        ////////////////////////////////////////
+
         body: Stack(
           children: [
+            /////////////////// BODY ////////////////////////
             Container(
               color: const Color.fromARGB(255, 131, 160, 126),
-            ),
-            Positioned(
-              top: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                height: 300,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 165, 204, 158),
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(0),
-                    topRight: Radius.circular(0),
-                    bottomLeft: Radius.circular(150),
-                    bottomRight: Radius.circular(150),
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.5),
-                      spreadRadius: 3.0,
-                      blurRadius: 8,
-                      offset: const Offset(0, 4),
-                    )
-                  ],
-                ),
+              child: Row(
+                ///////////////// PESTAÑAS //////////////////////
+                children: [Container()],
+
+                ////////////////////////////////////////////////
               ),
+
+              //////////////// GALERIA ///////////////////////
+
+              ////////////////////////////////////////////////
             ),
-            Positioned(
-              top: 20,
-              left: 0,
-              right: 0,
-              child: Center(
-                child: Stack(
-                  children: [
-                    Center(
-                      child: Container(
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          gradient: LinearGradient(
-                            colors: [
-                              Color.fromARGB(255, 41, 46, 40),
-                              Color.fromARGB(255, 68, 105, 63),
-                              Color.fromARGB(255, 113, 168, 105),
-                              Color.fromARGB(255, 158, 224, 147),
-                              Color.fromARGB(255, 161, 231, 150),
-                              Color.fromARGB(255, 166, 241, 154),
-                            ],
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
+
+            ///// CABECERA PERFIL /////
+            Column(
+              children: <Widget>[
+                Container(
+                  height: 300,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 165, 204, 158),
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(0),
+                      topRight: Radius.circular(0),
+                      bottomLeft: Radius.circular(150),
+                      bottomRight: Radius.circular(150),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.5),
+                        spreadRadius: 3.0,
+                        blurRadius: 8,
+                        offset: const Offset(0, 4),
+                      )
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        mainAxisSize: MainAxisSize.max,
+                        children: <Widget>[
+                          IconButton(
+                            icon: const Icon(Icons.notifications_sharp),
+                            iconSize: 40,
+                            color: Colors.white,
+                            onPressed: () {
+                              // Acción que se ejecutará al presionar el icono
+                            },
                           ),
-                        ),
-                        width: 170,
-                        height: 170,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black.withOpacity(0.2),
-                                spreadRadius: 3.0,
-                                blurRadius: 3,
-                                offset: const Offset(0, 4),
+                          Container(
+                            margin: const EdgeInsets.all(10),
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              gradient: LinearGradient(
+                                colors: [
+                                  Color.fromARGB(255, 41, 46, 40),
+                                  Color.fromARGB(255, 68, 105, 63),
+                                  Color.fromARGB(255, 113, 168, 105),
+                                  Color.fromARGB(255, 158, 224, 147),
+                                  Color.fromARGB(255, 161, 231, 150),
+                                  Color.fromARGB(255, 166, 241, 154),
+                                ],
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter,
                               ),
-                            ],
-                          ),
-                          child: const Center(
-                            child: CircleAvatar(
-                              radius: 80,
-                              backgroundImage: NetworkImage(
-                                  "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/980.jpg"),
+                            ),
+                            width: 170,
+                            height: 170,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.2),
+                                    spreadRadius: 3.0,
+                                    blurRadius: 3,
+                                    offset: const Offset(0, 4),
+                                  ),
+                                ],
+                              ),
+                              child: const Center(
+                                child: CircleAvatar(
+                                  radius: 80,
+                                  backgroundImage: NetworkImage(
+                                      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/980.jpg"),
+                                ),
+                              ),
                             ),
                           ),
+                          IconButton(
+                            icon: const Icon(Icons.message),
+                            iconSize: 40,
+                            color: Colors.white,
+                            onPressed: () {
+                              // Acción que se ejecutará al presionar el icono
+                            },
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Text(
+                            "@IvanSM",
+                            style: TextStyle(
+                              fontSize: 32,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              shadows: [
+                                Shadow(
+                                  offset: const Offset(3.0, 3.0),
+                                  blurRadius: 6.0,
+                                  color: Colors.black.withOpacity(0.5),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(8),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              icon: const Icon(Icons.tiktok_outlined),
+                              iconSize: 40,
+                              color: Colors.white,
+                              onPressed: () {
+                                // Acción que se ejecutará al presionar el icono
+                              },
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.facebook),
+                              iconSize: 40,
+                              color: Colors.white,
+                              onPressed: () {
+                                // Acción que se ejecutará al presionar el icono
+                              },
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.telegram),
+                              iconSize: 40,
+                              color: Colors.white,
+                              onPressed: () {
+                                // Acción que se ejecutará al presionar el icono
+                              },
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.telegram),
+                              iconSize: 40,
+                              color: Colors.white,
+                              onPressed: () {
+                                // Acción que se ejecutará al presionar el icono
+                              },
+                            ),
+                          ],
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
+              ],
             ),
-            Positioned(
-              top: 70,
-              right: 30,
-              child: IconButton(
-                icon: const Icon(Icons.message),
-                iconSize: 50,
-                color: Colors.white,
-                onPressed: () {
-                  // Acción que se ejecutará al presionar el icono de editar
-                },
-              ),
-            ),
-            Positioned(
-              top: 70,
-              left: 30,
-              child: IconButton(
-                icon: const Icon(Icons.notifications_sharp),
-                iconSize: 50,
-                color: Colors.white,
-                onPressed: () {
-                  // Acción que se ejecutará al presionar el icono de editar
-                },
-              ),
-            ),
-            Positioned(
-              top: 200,
-              left: 125,
-              child: Text(
-                "@IvanSM",
-                style: TextStyle(
-                  fontSize: 32,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  shadows: [
-                    Shadow(
-                      offset: const Offset(3.0, 3.0),
-                      blurRadius: 6.0,
-                      color: Colors.black.withOpacity(0.5),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Container()
+
+            //////////////////////////////////////////////////
           ],
         ),
       ),
